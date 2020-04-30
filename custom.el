bc53be1e4182ebe0f5075868fadecd6a9e0500cf
Siddharth Kannan <mail@siddharthkannan.in>
2020-04-30 23:29:20 +0900

    Always use commit when getting git link

--- custom.el
+++ custom.el
@@ -3,9 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(git-link-use-commit t)
  '(package-selected-packages
    (quote
-	(editorconfig helm-projectile plantuml-mode general markdown-mode use-package helm go-mode go-mode\.el solarized-theme gnu-elpa-keyring-update evil))))
+	(git-link editorconfig helm-projectile plantuml-mode general markdown-mode use-package helm go-mode go-mode\.el solarized-theme gnu-elpa-keyring-update evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
