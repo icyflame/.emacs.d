92a0c206677eeb282583668364d38831a4cccdb0
Siddharth Kannan <mail@siddharthkannan.in>
2020-07-12 01:03:01 +0900

    org-roam-graph-show is deprecated after org-roam v1.0

--- init.el
+++ init.el
@@ -313,7 +313,7 @@ re-downloaded in order to locate PACKAGE."
   :bind (:map org-roam-mode-map
 			  (("C-c n l" . org-roam)
 			   ("C-c n f" . org-roam-find-file)
-			   ("C-c n g" . org-roam-graph-show))
+			   ("C-c n g" . org-roam-graph))
 			  :map org-mode-map
 			  (("C-c n i" . org-roam-insert))
 			  (("C-c n I" . org-roam-insert-immediate))))
