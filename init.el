df2d1f2d889a170670e13106244c4bc42d9b3339
Siddharth Kannan <mail@siddharthkannan.in>
2021-03-01 00:07:32 +0900

    Don't use newlines before closing paren

--- init.el
+++ init.el
@@ -66,8 +66,7 @@ re-downloaded in order to locate PACKAGE."
   (setq evil-motion-state-modes nil)
   :config
   (evil-mode)
-  (evil-ex-define-cmd "q" 'kill-this-buffer)
-  )
+  (evil-ex-define-cmd "q" 'kill-this-buffer))
 
 (defun kannan/ask-user-approval (prompt)
   "A function to ask the user for approval"
