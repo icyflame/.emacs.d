47d625ca2501e541c80868f2a0cf301cba55fd6b
Siddharth Kannan <mail@siddharthkannan.in>
2022-06-10 13:14:07 +0900

    custom: Add some changes to custom.el

--- custom.el
+++ custom.el
@@ -8,10 +8,29 @@
  '(helm-ag-success-exit-status '(0 2))
  '(helm-completion-style 'emacs)
  '(lsp-ui-doc-position 'bottom)
+ '(mml-secure-key-preferences
+   '((OpenPGP
+	  (sign)
+	  (encrypt
+	   ("mail@siddharthkannan.in" "71DB9964695BBCD235B5155305A6BC36250D69FF")))
+	 (CMS
+	  (sign)
+	  (encrypt))))
+ '(notmuch-saved-searches
+   '((:name "inbox" :query "tag:inbox" :key "i")
+	 (:name "unread" :query "tag:unread" :key "u")
+	 (:name "flagged" :query "tag:flagged" :key "f")
+	 (:name "sent" :query "tag:sent" :key "t")
+	 (:name "drafts" :query "tag:draft" :key "d")
+	 (:name "all mail" :query "*" :key "a")
+	 (:name "todo" :query "tag:todo")))
+ '(notmuch-search-oldest-first nil)
  '(org-agenda-files
    '("~/work/notes/TODO.org" "~/work/notes/Current.org" "~/personal/notes/Current.org" "~/personal/notes/Current.org"))
+ '(org-roam-directory "~/work/notes/org-roam")
  '(package-selected-packages
-   '(color-theme-tomorrow lsp-treemacs treemacs-projectile treemacs-evil treemacs "treemacs" nov org-journal atomic-chrome nord-theme notmuch awesome-tab web-mode ob-perl ob-go elfeed ox-hugo ox-confluence helm-rg yasnippet protobuf-mode org-roam-doctor gotest org-ref org-roam-bibtex org-roam-completion org-roam company-lsp lsp-mode company company-mode magit ggtags json-mode php-mode helm-ag powerline jsx-mode yaml-mode git-link sqlformat editorconfig helm-projectile plantuml-mode general markdown-mode use-package helm go-mode go-mode\.el solarized-theme gnu-elpa-keyring-update evil)))
+   '(rust-mode olivetti color-theme-tomorrow lsp-treemacs treemacs-projectile treemacs-evil treemacs "treemacs" nov org-journal lsp-ui solarized-theme atomic-chrome nord-theme notmuch awesome-tab web-mode ob-perl ob-go elfeed ox-hugo ox-confluence helm-rg yasnippet protobuf-mode org-roam-doctor gotest org-ref org-roam-bibtex org-roam-completion org-roam company-lsp lsp-mode company company-mode magit ggtags json-mode php-mode helm-ag powerline jsx-mode yaml-mode git-link sqlformat editorconfig helm-projectile plantuml-mode general markdown-mode use-package helm go-mode go-mode\.el gnu-elpa-keyring-update evil))
+ '(send-mail-function 'sendmail-send-it))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
