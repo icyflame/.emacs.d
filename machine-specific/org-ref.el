2485958505047779429362e19c74f1636ba46a00
Siddharth Kannan <mail@siddharthkannan.in>
2022-07-07 16:45:04 +0900

    Indent several elisp files with the default configuration
    I listed the elisp files by the number of lines they have.
    
    $ fd --glob '*.el' . | xargs wc -l | sort -rh
       13483 total
        2005 ./lisp/mozc.el
        1957 ./lisp/awesome-tab/awesome-tab.el
        1119 ./lisp/org-roam/org-roam-node.el
        1105 ./init.el
         881 ./lisp/fill-column-indicator.el
         829 ./lisp/org-roam/org-roam-capture.el
         789 ./lisp/org-roam/org-roam-db.el
         697 ./lisp/org-roam/org-roam-mode.el
         609 ./lisp/color-theme-tomorrow.el
         494 ./lisp/org-roam/org-roam-utils.el
         482 ./lisp/simpleclip.el
         366 ./lisp/org-roam/extensions/org-roam-dailies.el
         342 ./lisp/org-roam/org-roam.el
         302 ./lisp/org-roam/extensions/org-roam-graph.el
         241 ./lisp/org-roam/org-roam-compat.el
         174 ./lisp/org-roam/extensions/org-roam-protocol.el
         168 ./lisp/org-roam/org-roam-migrate.el
         146 ./imported-confs/10_utils.el
         117 ./lisp/org-roam/org-roam-id.el
          96 ./lisp/org-roam/extensions/org-roam-overlay.el
          83 ./lisp/org-roam/tests/test-org-roam.el
          81 ./snippets/yasnippet-snippets/yasnippet-snippets.el
          75 ./lisp/org-roam/extensions/org-roam-export.el
          56 ./lisp/org-roam/tests/test-org-roam-capture.el
          53 ./machine-specific/org-roam.el
          53 ./lisp/org-roam/org-roam-log.el
          39 ./lisp/org-roam/tests/test-org-roam-utils.el
          39 ./custom.el
          34 ./imported-confs/31_plantuml_preview_function.el
          33 ./imported-confs/30_plantuml_helpers.el
          18 ./machine-specific/org-ref.el

--- machine-specific/org-ref.el
+++ machine-specific/org-ref.el
@@ -1,18 +1,18 @@
 (require-package 'org-ref)
 (use-package org-ref
-  :config
-  ;; Enable downloading PDFs / getting bibtex entries using DOI
-  (require 'doi-utils)
-  (require 'org-ref-arxiv)
+    :config
+    ;; Enable downloading PDFs / getting bibtex entries using DOI
+    (require 'doi-utils)
+    (require 'org-ref-arxiv)
 
-  (setq reftex-default-bibliography '("~/personal/notes/bibliography/references.bib"))
-  ;; Required for org-ref
-  (setq org-ref-bibliography-notes "~/personal/notes/bibliography/notes.org"
-		org-ref-default-bibliography '("~/personal/notes/bibliography/references.bib")
-		org-ref-pdf-directory "~/personal/notes/bibliography/bibtex-pdfs/")
+    (setq reftex-default-bibliography '("~/personal/notes/bibliography/references.bib"))
+    ;; Required for org-ref
+    (setq org-ref-bibliography-notes "~/personal/notes/bibliography/notes.org"
+        org-ref-default-bibliography '("~/personal/notes/bibliography/references.bib")
+        org-ref-pdf-directory "~/personal/notes/bibliography/bibtex-pdfs/")
 
-  ;; Required for helm-bibtex
-  (setq bibtex-completion-bibliography "~/personal/notes/bibliography/references.bib"
-		bibtex-completion-library-path "~/personal/notes/bibliography/bibtex-pdfs"
-		bibtex-completion-notes-path "~/personal/notes/bibliography/helm-bibtex-notes")
-  )
+    ;; Required for helm-bibtex
+    (setq bibtex-completion-bibliography "~/personal/notes/bibliography/references.bib"
+        bibtex-completion-library-path "~/personal/notes/bibliography/bibtex-pdfs"
+        bibtex-completion-notes-path "~/personal/notes/bibliography/helm-bibtex-notes")
+    )
