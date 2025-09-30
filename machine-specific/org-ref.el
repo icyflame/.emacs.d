723fe2b89521879774271d52b37381776f06fc7d
Siddharth Kannan <mail@siddharthkannan.in>
2025-10-01 00:00:10 +0900

    fix: Defer all packages related to org-ref

--- machine-specific/org-ref.el
+++ machine-specific/org-ref.el
@@ -1,8 +1,11 @@
 (use-package ivy-bibtex
+    :defer t
     :ensure t)
 (use-package pdf-tools
+    :defer t
     :ensure t)
 (use-package org-ref
+    :defer t
     :ensure t
     :config
     ;; Enable downloading PDFs / getting bibtex entries using DOI
@@ -18,5 +21,4 @@
     ;; Required for helm-bibtex
     (setq bibtex-completion-bibliography "~/notes/bibliography/references.bib"
         bibtex-completion-library-path "~/notes/bibliography/bibtex-pdfs"
-        bibtex-completion-notes-path "~/notes/bibliography/ivy-bibtex-notes")
-    )
+        bibtex-completion-notes-path "~/notes/bibliography/ivy-bibtex-notes"))
