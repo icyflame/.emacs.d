980fcf868af86c3d8bebea9ff12c0e9255db0306
Siddharth Kannan <mail@siddharthkannan.in>
2025-10-21 09:49:29 +0900

    feat: Stop using custom.el for these variables

--- custom.el
+++ custom.el
@@ -25,9 +25,6 @@
 	 (:name "all mail" :query "*" :key "a")
 	 (:name "todo" :query "tag:todo")))
  '(notmuch-search-oldest-first nil)
- '(org-agenda-files
-   '("~/work/notes/TODO.org" "~/work/notes/Current.org" "~/personal/notes/Current.org" "~/personal/notes/Current.org"))
- '(org-roam-directory "~/work/notes/org-roam")
  '(package-selected-packages
    '(filenotify-recursive emacsql-sqlite org-roam-id rust-mode olivetti color-theme-tomorrow lsp-treemacs treemacs-projectile treemacs-evil treemacs "treemacs" nov org-journal lsp-ui solarized-theme atomic-chrome nord-theme notmuch awesome-tab web-mode ob-perl ob-go elfeed ox-hugo ox-confluence helm-rg yasnippet protobuf-mode org-roam-doctor gotest org-ref org-roam-bibtex org-roam-completion org-roam company-lsp lsp-mode company company-mode magit ggtags json-mode php-mode helm-ag powerline jsx-mode yaml-mode git-link sqlformat editorconfig helm-projectile plantuml-mode general markdown-mode use-package helm go-mode go-mode\.el gnu-elpa-keyring-update evil))
  '(send-mail-function 'sendmail-send-it))
